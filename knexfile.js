4b9ac9ea61056831fe35983ebeb9f698629d3ad2
Erik Davtyan <erikdavtyan@gmail.com>
2022-01-30 18:46:30 +0400

    Add production knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,4 +14,19 @@ module.exports = {
       },
     },
   },
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/tasks.db",
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    pool: {
+      afterCreate: (conn, cb) => {
+        conn.run("PRAGMA foreign_keys = ON", cb);
+      },
+    },
+  },
 };
